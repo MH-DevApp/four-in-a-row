fd6ebb5ed3909f82ed2c48c588837de37d4c1c6f
mehdi <mehdih.devapp@gmail.com>
2024-02-21 22:32:42 +0100

    feat: toggle theme and save in localStorage

--- single/src/main.ts
+++ single/src/main.ts
@@ -1,4 +1,24 @@
 import "./style.css";
 import { GameBoardInstance } from "./gameboard/GameBoard.ts";
 
-GameBoardInstance.start();
+if (localStorage.getItem("theme") && localStorage.getItem("theme") === "dark") {
+  document.documentElement.classList.add("dark");
+}
+
+const darkMode = document.querySelector("#darkMode");
+const lightMode = document.querySelector("#lightMode");
+
+if (darkMode && lightMode) {
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem(
+      "theme",
+      document.documentElement.classList.contains("dark") ? "dark" : "light",
+    );
+  };
+
+  darkMode.addEventListener("click", toggleTheme);
+  lightMode.addEventListener("click", toggleTheme);
+}
+
+GameBoardInstance.init();
