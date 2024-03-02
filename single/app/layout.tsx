eeaa1a9dcd64d98ebc78b21ac6cfdc18a2d012d7
mehdi <mehdih.devapp@gmail.com>
2024-03-02 19:16:19 +0100

    feat: Footer component

--- single/app/layout.tsx
+++ single/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { poppins } from "@/utils/fonts";
 import HeaderComponent from "@/components/HeaderComponent";
+import FooterComponent from "@/components/FooterComponent";
 
 export const metadata: Metadata = {
   title: "Four In A Row",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <HeaderComponent />
         {children}
+        <FooterComponent />
       </body>
     </html>
   );
---
+++ single/src/components/FooterComponent.tsx
@@ -0,0 +1,9 @@
+const FooterComponent = () => {
+  return (
+    <footer className="border-t-2 border-t-slate-400 py-4 text-center text-xs text-slate-500 dark:bg-slate-800 dark:text-slate-200">
+      MH-Dev &copy; - 4 In A Row / Challenge project - 2024
+    </footer>
+  );
+};
+
+export default FooterComponent;
